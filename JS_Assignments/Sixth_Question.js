85a511faf1ad20c6f1a494b88f7d73ebd4e8ab1a
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2023-08-24 16:01:05 +0530

    Delete JS_Assignments directory

--- JS_Assignments/Sixth_Question.js
+++
@@ -1,5 +0,0 @@
-// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
-
-let givenString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
-let resultedArray = givenString.split(",");
-console.log(resultedArray);
--- JS_Assignments/Third_Question.js
+++
@@ -1,6 +0,0 @@
-// 03. Declare a variable and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
-
-let langName = "Javascript";
-let capitalize = langName.toUpperCase();
-
-console.log(capitalize);
