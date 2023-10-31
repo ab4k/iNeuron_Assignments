67040176f295c16e1bd109fe662514bfae9ec17e
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2023-11-01 02:02:15 +0530

    Delete DOM_Assignment_7 directory

--- DOM_Assignment_7/solution/task1.js
+++
@@ -1,9 +0,0 @@
-// Assignment - 1: Remove the languages that have 2.0 in their name(Every alternative language)
-
-const links = document.querySelectorAll("a");
-
-links.forEach((li) => {
-  if (li.innerHTML.includes("2.0")) {
-    li.innerHTML = "";
-  }
-});
