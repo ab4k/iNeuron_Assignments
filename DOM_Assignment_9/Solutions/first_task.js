16eeb2579fa99752ff60b8d0f0a9a0c3462f2588
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2023-11-01 02:01:49 +0530

    Delete DOM_Assignment_9 directory

--- DOM_Assignment_9/Solutions/first_task.js
+++
@@ -1,3 +0,0 @@
-const h1 = document.querySelector("h1");
-h1.style.color = "#DC143C";
-h1.style.fontFamily = "Times new roman";
--- DOM_Assignment_9/Solutions/second_task.js
+++
@@ -1,9 +0,0 @@
-const addToCartBtn = document.querySelector(".add-to-cart");
-const btnOnHover = () => {
-  addToCartBtn.style.backgroundColor = "#DC143C";
-};
-const btnOriginal = () => {
-  addToCartBtn.style.backgroundColor = "hsl(158, 36%, 37%)";
-};
-addToCartBtn.addEventListener("mouseover", btnOnHover);
-addToCartBtn.addEventListener("mouseout", btnOriginal);
