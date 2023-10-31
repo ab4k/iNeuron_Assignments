fd0a5a5c6da1e0fb6dd268bbf1f13be47cc83658
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2023-11-01 01:32:52 +0530

    Add files via upload

---
+++ DOM_Assignment_6/solution/task1.js
@@ -0,0 +1,4 @@
+const logo = document.querySelector(".logo");
+logo.src = "../originalFiles/assets/ineuron-logo.png";
+logo.style.width = "500px";
+document.querySelector("main").style.padding = "100px";
