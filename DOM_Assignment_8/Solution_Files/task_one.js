0bd5d76637f969a760d90d6e2b8d2ea5f175483d
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2023-11-01 02:02:29 +0530

    Delete DOM_Assignment_8 directory

--- DOM_Assignment_8/Solution_Files/task_one.js
+++
@@ -1,4 +0,0 @@
-const box = document.querySelector(".new");
-box.style.width = "300px";
-box.style.height = "440px";
-box.style.overflow = "auto";
--- DOM_Assignment_8/Solution_Files/task_three.js
+++
@@ -1,9 +0,0 @@
-const hamburgerIcon = document.querySelector(".navbar-toggler-icon");
-const menu = document.getElementById("navbarTogglerDemo01");
-hamburgerIcon.addEventListener("click", () => {
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-});
